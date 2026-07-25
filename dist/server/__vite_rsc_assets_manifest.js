4d3e431e4c77524f2bb4ce3d0ec819c2173f89c8
Dorianpmy <dorian.poumay10@gmail.com>
2026-07-25 12:09:19 +0200

    Add Sites deployment manifest

---
+++ dist/server/__vite_rsc_assets_manifest.js
@@ -0,0 +1,56 @@
+export default {
+  clientReferenceDeps: {
+    "593f344dc510": {
+      js: [],
+      css: [],
+    },
+    "15c18cfaeeff": {
+      js: [
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-CXnKph_e.js",
+        "/assets/index-BwMw9lbC.js",
+        "/assets/layout-segment-context-BEBZBNG5.js",
+      ],
+      css: [],
+    },
+    "8c0f216c4604": {
+      js: [
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-CXnKph_e.js",
+        "/assets/index-BwMw9lbC.js",
+      ],
+      css: [],
+    },
+    "6efdf509a785": {
+      js: [
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-CXnKph_e.js",
+        "/assets/index-BwMw9lbC.js",
+        "/assets/page-CFFRsGOh.js",
+      ],
+      css: ["/assets/page-vh-t_kPv.css"],
+    },
+    "0090da9e0fa3": {
+      js: [
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-CXnKph_e.js",
+        "/assets/index-BwMw9lbC.js",
+      ],
+      css: [],
+    },
+    "d898cce2865f": {
+      js: [
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-CXnKph_e.js",
+        "/assets/index-BwMw9lbC.js",
+      ],
+      css: [],
+    },
+  },
+  serverResources: {
+    "app/layout.tsx": {
+      js: [],
+      css: ["/assets/index-BACOlZvc.css", "/assets/page-vh-t_kPv.css"],
+    },
+  },
+};
